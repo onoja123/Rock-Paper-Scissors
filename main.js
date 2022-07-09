d6338e9bf4c77cbe754c0e7f5a8a795528406820
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2022-07-10 00:11:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,58 @@
+// Get player text field
+const playerText = document.querySelector("#playerText");
+// Get computer text field
+const computerText = document.querySelector("#computerText");
+// Get result text field
+const resultText = document.querySelector("#resultText");
+// Get all buttons
+const choiceBtns = document.querySelectorAll(".choiceBtn")
 
+
+let player;
+let computer;
+let result;
+
+//Get each buttons and add event listeners
+choiceBtns.forEach(button => button.addEventListener("click", ()=>{
+    player = button.textContent;
+    computerTurn();
+    playerText.textContent = 'player: ${player}' 
+    computerText.textContent = 'computer: ${computer}' 
+    resultText.textContent = checkWinner()
+}))
+
+// computer logic here
+function computerTurn(){
+    const randomNumber = Math.floor(Math.random() * 3) +1;
+
+    switch (randomNumber) {
+        case 1:
+            computer = "Rock"
+            break;
+    
+        case 2:
+            computer = "Papper"
+            break;
+
+        case 3:
+            computer = "Scissors"
+            break;    
+
+        default:
+            break;
+    }
+}
+
+//check winner logic
+
+function checkWinner(){
+    if(player == computer){
+        return "Draw!"
+    }else if(computer == "Rock"){
+        result(player == "Paper") ? "You Win" : "You Loose"
+    }else if(computer == "Paper"){
+        return(player =="Scissors") ? "You Win" : "You loose"
+    }else if(computer == "Scissors"){
+        return(player == "Rock") ? "You Win " : "You Loose "
+    }
+}
